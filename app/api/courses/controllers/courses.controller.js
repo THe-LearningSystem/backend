19418990cc55db8f05f26588ec46d1a26156d23f
MRisto <risto.matthias@gmail.com>
2017-05-02 21:50:45 +0200

    Refactored course controller, started lessons

--- app/api/courses/controllers/courses.controller.js
+++ app/api/courses/controllers/courses.controller.js
@@ -3,7 +3,8 @@
 /**
  * Module dependencies
  */
-var mongoose = require('mongoose'),
+var _ = require('lodash'),
+    mongoose = require('mongoose'),
     Course = mongoose.model('Course');
 
 
@@ -46,39 +47,85 @@ exports.getAll = function (req, res) {
         });
 };
 
-/**
- * Get one course with a given urlName
- */
-exports.getOne = function (req, res) {
+var _getOneCourse = function(req,res,callback){
     var id = req.params.courseId;
     if(mongoose.Types.ObjectId.isValid(id)){
         Course
             .findOne({_id: id})
             .select('-__v')
-            .exec(function (err, doc) {
+            .exec(function (err, course) {
                 if (err) {
                     console.log(err);
                     res.status(500).send(err);
-                } else if (!doc) {
+                } else if (!course) {
                     res.status(404).send({msg: "course id not found " + id});
                 } else {
-                    res.send(doc);
+                  callback(course);
                 }
             });
     }else{
         Course
             .findOne({urlName: id})
             .select('-__v')
-            .exec(function (err, doc) {
+            .exec(function (err, course) {
                 if (err) {
                     console.log(err);
                     res.status(500).send(err);
-                } else if (!doc) {
+                } else if (!course) {
                     res.status(404).send({msg: "course id not found " + id});
                 } else {
-                    res.send(doc);
+                    callback(course);
                 }
             });
     }
+};
 
+/**
+ * Get one course with a given urlName
+ */
+exports.getOne = function (req, res) {
+    _getOneCourse(req,res,function(course){
+        res.send(course);
+    });
+};
+
+exports.update = function(req,res){
+    _getOneCourse(req,res,function(course){
+        course.name =req.body.name;
+        course.description = req.body.description;
+        course.sections = req.body.sections;
+        course.save(function (err) {
+            if (err) {
+                res.send({msg:'Couldnt update Course'});
+            } else {
+                res.send({msg: "Updated Course"});
+            }
+        })
+    });
+};
+
+var _addSection = function (req, res, course) {
+    if (_.find(course.sections, {name: req.body.name}) === undefined) {
+        course.sections.push(req.body);
+        course.save(function (err) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send({msg: "Section Created"});
+            }
+        })
+    } else {
+        res.send({msg: "name '" + req.body.name + "' is not unique"});
+    }
+};
+
+
+
+/**
+ * Sections
+ */
+exports.createSection = function(req,res){
+    _getOneCourse(req,res,function(course){
+        _addSection(req,res,course);
+    });
 };
